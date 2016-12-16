5d10f484a93b141d761c27986d84443991034777
Ronald Regalado <regalado.ronald@hotmail.com>
2016-12-16 16:10:08 +0000

    final1

--- app_client/app.js
+++ app_client/app.js
@@ -9,7 +9,7 @@ function config($routeProvider, uiGmapGoogleMapApiProvider) {
         v: '3.20', //defaults to latest 3.X anyhow
         libraries: 'weather,geometry,visualization'
     });
-//I see you devonte
+
     $routeProvider
         .when('/', {
             templateUrl: '/home/home.view.html',
